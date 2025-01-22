9c4695380b6066cf189c31da7d6503b348b6d36d
Inba-11 <inbatamiz11@gmail.com>
2025-01-22 22:57:49 +0530

    Time: 0 ms (100%), Space: 9 MB (9.15%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n=s.size();
+        int length=0;
+        for(int i=n-1;i>=0;i--){
+            if(s[i]!=' '){
+                length++;
+            }else if(length>0){
+                break;
+            }
+        }
+        return length;
+    }
+};
